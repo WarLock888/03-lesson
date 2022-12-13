6841ad3a9c8ec18865bb051750bb3b063c38c87f
MaksimNovozhilov <ufamax@gmail.com>
2022-12-13 14:17:51 +0600

    module 1

--- 1seq.py
+++ 1seq.py
@@ -1,11 +1,10 @@
+num_count = int(input('Количество цифр: '))  # спрашиваем количесвто чисел
 
-num_count = int(input('Количество цифр: ')) # спрашиваем количесвто чисел
-
-my_list = [] # создаем пустой список
-for i in range(num_count): # запрашиваем числа по очереди
-    num = int(input(f'Введите число({i+1}): '))
+my_list = []  # создаем пустой список
+for i in range(num_count):  # запрашиваем числа по очереди
+    num = int(input(f'Введите число({i + 1}): '))
     my_list.append(num)
 
-my_list.sort() # сортируем список
+my_list.sort()  # сортируем список
 
-print(my_list) #  вывод
+print(my_list)  # ввывод
